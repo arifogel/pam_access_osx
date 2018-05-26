05c1f2adcdfad700794b8ed311576ed643e28618
Ari Fogel <ari@fogelti.me>
2018-05-25 22:57:44 -0700

    ip utils probably work

--- src/ip_util.c
+++ src/ip_util.c
@@ -1,49 +1,69 @@
-#include <ip_util.h>
+#include <string.h>
 
-uint32_t __ip(const char* addr_str) {
-  uint32_t ip_network_order;
-  inet_pton(AF_INET, addr_str, &ip_network_order);
-  return ntohl(ip_network_order);
-}
+#include "ip_util.h"
 
-struct in6_addr __ip6(const char* addr_str) {
-  struct in6_addr  ip_network_order;
-  inet_pton(AF_INET6, addr_str, &ip_network_order);
-  int i;
-  struct in6_addr output;
-  uint8_t* output_bytes_network_order = (uint8_t*)&ip_network_order;
-  uint8_t* output_bytes = (uint8_t*)&output;
-  for (i = 0; i < 128; i++) {
-    output_bytes[i] = output_bytes_network_order[127-i];
-  }
-  return output;
+struct in6_addr
+__ip6(
+  const char* addr_str) {
+  ipv6_addr_t addr;
+  inet_pton(AF_INET6, addr_str, &addr);
+  return addr;
 }
 
 bool
 ipv4_network_contains(
   ipv4_network_t network,
-  uint32_t address) {
-  uint32_t mask;
+  ipv4_addr_t address) {
+  ipv4_addr_t mask;
   netmask4(network.length, &mask);
-  uint32_t masked_network = network.address & mask;
-  uint32_t masked_address = address & mask;
+  ipv4_addr_t masked_network = network.address & mask;
+  ipv4_addr_t masked_address = address & mask;
   return masked_network == masked_address;
 }
 
-//bool
-//ipv6_network_contains(
-//  ipv6_network_t network,
-//  struct in6_addr address){
-//
-//}
+bool
+ipv6_network_contains(
+  ipv6_network_t network,
+  ipv6_addr_t address) {
+  ipv6_addr_t mask;
+  netmask6(network.length, &mask);
+  ipv6_addr_t masked_network = network.address;
+  ((uint64_t*) &masked_network)[0] &= ((uint64_t*) &mask)[0];
+  ((uint64_t*) &masked_network)[1] &= ((uint64_t*) &mask)[1];
+  ipv6_addr_t masked_address = address;
+  ((uint64_t*) &masked_address)[0] &= ((uint64_t*) &mask)[0];
+  ((uint64_t*) &masked_address)[1] &= ((uint64_t*) &mask)[1];
+  return ((uint64_t*) &masked_network)[0] == ((uint64_t*) &masked_address)[0]
+      && ((uint64_t*) &masked_network)[1] == ((uint64_t*) &masked_address)[1];
+}
 
 bool
-netmask4(uint8_t length, uint32_t* mask) {
+netmask4(
+  uint8_t length,
+  ipv4_addr_t* mask) {
   if (length > 32) {
     return false;
   }
   int shift = 32 - length;
-  *mask = ~'0' >> shift << shift;
+  *mask = htonl(~((ipv4_addr_t )0) >> shift << shift);
   return true;
 }
 
+bool
+netmask6(
+  uint8_t length,
+  ipv6_addr_t* mask) {
+  if (length > 128) {
+    return false;
+  }
+  uint8_t* bytes = (uint8_t*) mask;
+  int full_bytes = length / 8;
+  int remaining_bits = length % 8;
+  memset(bytes, 0xFF, full_bytes);
+  memset(bytes + full_bytes, 0x00, 16 - full_bytes);
+  if (remaining_bits) {
+    int shift = 8 - remaining_bits;
+    bytes[full_bytes] = ((uint8_t) 0xFF) >> shift << shift;
+  }
+  return true;
+}
--- tests/test_ip_network.c
+++ tests/test_ip_network.c
@@ -8,20 +8,38 @@ int
 main(
   void) {
   // ipv4_network tests
-  ipv4_network_t n4 = { .address = __ip("192.168.0.0"), .length = 24 };
+  ipv4_network_t n4_1 = { .address = inet_addr("192.168.0.0"), .length = 24 };
 
-  ipv4_addr_t addr_in = __ip("192.168.0.1");
-  ipv4_addr_t addr_out = __ip("192.168.1.1");
+  ipv4_addr_t addr_in_1 = inet_addr("192.168.0.1");
+  ipv4_addr_t addr_out_1 = inet_addr("192.168.1.1");
 
-  assert(ipv4_network_contains(n4, addr_in));
-  assert(!ipv4_network_contains(n4, addr_out));
+  assert(ipv4_network_contains(n4_1, addr_in_1));
+  assert(!ipv4_network_contains(n4_1, addr_out_1));
+
+  // length not divisible by 8
+  ipv4_network_t n4_2 = { .address = inet_addr("1.2.3.4"), .length = 30 };
+
+  ipv4_addr_t addr_in_2 = inet_addr("1.2.3.5");
+  ipv4_addr_t addr_out_2 = inet_addr("1.2.3.0");
+
+  assert(ipv4_network_contains(n4_2, addr_in_2));
+  assert(!ipv4_network_contains(n4_2, addr_out_2));
 
   // ipv6_network tests
-  ipv6_network_t n6 = {.address = __ip6("3::"), .length = 64 };
+  ipv6_network_t n6_1 = {.address = __ip6("3::"), .length = 64 };
 
-  ipv6_addr_t addr6_in = __ip6("3::1");
-  ipv6_addr_t addr6_out = __ip6("3:1::1");
+  ipv6_addr_t addr6_in_1 = __ip6("3::1");
+  ipv6_addr_t addr6_out_1 = __ip6("3:1::1");
 
-  assert(ipv6_network_contains(n6, addr6_in));
-  assert(!ipv6_network_contains(n6, addr6_out));
+  assert(ipv6_network_contains(n6_1, addr6_in_1));
+  assert(!ipv6_network_contains(n6_1, addr6_out_1));
+
+  // length not divisible by 8
+  ipv6_network_t n6_2 = {.address = __ip6("1:2:3:4:5:6:7:4"), .length = 126 };
+
+  ipv6_addr_t addr6_in_2 = __ip6("1:2:3:4:5:6:7:5");
+  ipv6_addr_t addr6_out_2 = __ip6("1:2:3:4:5:6:7:0");
+
+  assert(ipv6_network_contains(n6_2, addr6_in_2));
+  assert(!ipv6_network_contains(n6_2, addr6_out_2));
 }
